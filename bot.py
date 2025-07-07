7a088f6c69aff2e7e549b7dc3e45d2505ceafa7b
zyrafaq <krzesimir@hobler.pl>
2025-07-07 10:05:26 +0200

    update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,21 @@ from player import Player
 
 
 class Bot(Player):
-    def __init__(self, strategy="get_all"):
+    def __init__(self, name=None, strategy="get_all", score_to_save=300):
         super().__init__()
+        self.name = name
         self.strategy = strategy
+        self.score_to_save = score_to_save
+
+    def __str__(self):
+        return "Bot"
+
+    def pick_figures(self, figures):
+        match self.strategy:
+            case "get_all":
+                return figures
+            case _:
+                raise Exception(f"Unknown strategy {self.strategy}")
+
+    def is_stop_condition_met(self, score_to_risk):
+        return score_to_risk >= self.score_to_save
