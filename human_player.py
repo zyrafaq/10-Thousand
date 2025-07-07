fe5375113527e560b41bc8703bee359aeafc8fdb
zyrafaq <krzesimir@hobler.pl>
2025-07-07 10:05:04 +0200

    create human player

---
+++ human_player.py
@@ -0,0 +1,39 @@
+from player import Player
+
+
+class HumanPlayer(Player):
+    def __init__(self, name=None):
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        if self.name:
+            return f"Human: {self.name}"
+        else:
+            return f"Human"
+    def pick_figures(self, figures):
+        if len(figures) == 0:
+            print("No figures. Sorry")
+            return []
+        if len(figures) == 1:
+            print("Only one figure. Using it")
+            return figures
+        chosen_figures = ()
+        while len(chosen_figures) == 0:
+            player_input = input("Which figures would you like to choose?")
+            if not player_input:
+                print("Please enter a valid input")
+                continue
+            indexes = [int(x.strip()) - 1 for x in player_input.split(',')]
+            chosen_figures = [figures[i] for i in indexes if 0 <= i < len(figures)]
+        return chosen_figures
+
+    def is_stop_condition_met(self, full_score_to_risk_and_current_score):
+        choice = input("Stop now? (t/N): ").strip().lower()
+        if choice == "t":
+            return True
+        elif choice == "n" or choice == "":
+            return False
+        else:
+            print("Invalid input. Please type 't' to stop or press Enter to continue.")
+            return self.is_stop_condition_met()
