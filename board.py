40fe914269d25d8826290a6140320fa36648d269
zyrafaq <krzesimir@hobler.pl>
2025-07-07 10:04:07 +0200

    update board.py

--- board.py
+++ board.py
@@ -1,10 +1,30 @@
+from constants import Constants
+import random
+import time
+
+
 class Board:
     def __init__(self, players):
         self.players = players
 
-    def play(self):
-        round = 1
-        for player in self.players:
-            print(f"Round: {round}")
-            player.roll()
-            round += 1
+    def play(self, randomize_starting_player=False):
+        round_number = 1
+        if randomize_starting_player:
+            starting_player = random.choice(self.players)
+            self.players.remove(starting_player)
+            self.players.insert(0, starting_player)
+        print(f"{self.players[0]} is the starting player")
+        while True:
+            print(f"Round: {round_number}")
+            for player in self.players:
+                print(f"\n{player}'s turn")
+                print(f"Current score: {player.score}")
+                score_gained = player.play()
+                print(f"{player} gained: {score_gained} score. Total score: {player.score}")
+
+                if player.score >= Constants.GOAL:
+                    print(f"\n{player} has reached the goal of {Constants.GOAL}!")
+                    print(f"{player} wins!")
+                    return
+                time.sleep(1.3)
+            round_number += 1
