9b40af71a353cb98f62a60fc3ec688cfca520df1
zyrafaq <krzesimir@hobler.pl>
2025-07-23 20:21:37 +0200

    remove unused code in main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from bot import Bot
 from human_player import HumanPlayer
 
 
-score_to_save = 0
-
 board = Board(players=[HumanPlayer(), Bot()])
 try:
     board.play(randomize_starting_player=True)
